78cd9765826c21bf966f7c0b09a97c91555221ae
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-05-23 12:31:23 +0900

    Spotify-Clone Project

---
+++ Spotify_Clone/Spotify_Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify_Clone
+//
+//  Created by 이영준 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spotify_Clone/Spotify_CloneUITests/Spotify_CloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Spotify_CloneUITestsLaunchTests.swift
+//  Spotify_CloneUITests
+//
+//  Created by 이영준 on 2022/05/23.
+//
+
+import XCTest
+
+class Spotify_CloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
